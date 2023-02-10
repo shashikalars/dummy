29e849e0507bacacb44bb5ae4581b83a8ae76956
Dhanaprakash Durairaj <dhdurairaj@rschulhof-ub18.inca.infoblox.com>
2023-02-10 01:20:29 -0800

    second commit

---
+++ WAPI_PyTest/suites/FTP_TFTP_HTTP/config.py
@@ -0,0 +1,69 @@
+"""
+Copyright (c) Infoblox Inc., 2022
+
+Description: This is Auto Generated config file.
+
+Author  : Vindya V K
+"""
+
+
+#LISTING ALL THE REQUIRED VARIABLES BELOW
+build="8.6.2-49947"
+grid1_session_ip="10.36.160.18"
+grid1_master_vip="10.35.160.18"
+grid1_master_fqdn="ib-10-35-160-18.infoblox.com"
+grid1_master_mgmt_vip="10.36.160.18"
+grid1_master_id="vm-18-39"
+grid1_master_ipv6="2001:0550:040A:2400:0000:0000:0000:A012"
+grid1_master_mgmt_vip6="2001:0550:040A:2500:0000:0000:0000:A012"
+owner="srs"
+grid1_netmask="255.255.0.0"
+grid1_netmask_mgmt="255.255.0.0"
+grid1_router="10.35.0.1"
+grid1_router_mgmt="10.36.0.1"
+grid1_router_ipv6="2001:0550:040A:2400:0000:0000:0000:0001"
+grid1_router_ipv6_mgmt="2001:0550:040A:2500:0000:0000:0000:0001"
+grid1_member1_ha_vip="10.34.6.254"
+grid1_member1_ha_vip_v6="2001:550:40A:2263::FE"
+grid1_member1_fqdn="ib-10-34-6-254.infoblox.com"
+grid1_member1_active_ha_ip="10.34.6.4"
+grid1_member1_passive_ha_ip="10.34.6.6"
+grid1_member1_active_ha_ipv6="2001:550:40A:2263::4"
+grid1_member1_passive_ha_ipv6="2001:550:40A:2263::6"
+grid1_member1_ha_active_id="vm-14-10"
+grid1_member1_ha_passive_id="vm-19-39"
+grid1_member2_vip="10.35.178.10"
+grid1_member2_fqdn="ib-10-35-178-10.infoblox.com"
+grid1_member2_id="vm-10-57"
+grid1_member2_ipv6="2001:550:40A:2400::B20A"
+grid1_member2_mgmt_ip="10.36.178.10"
+grid1_member2_mgmt_ipv6="2001:550:40A:2500::B20A"
+grid2_session_ip="10.35.152.2"
+grid2_master_vip="10.35.152.2"
+grid2_master_fqdn="ib-10-35-152-2.infoblox.com"
+grid2_master_mgmt_vip="10.36.152.2"
+grid2_master_id="vm-02-31"
+grid2_master_ipv6="2001:0550:040A:2400:0000:0000:0000:9802"
+grid2_master_mgmt_vip6="2001:0550:040A:2500:0000:0000:0000:9802"
+owner="srs"
+grid2_netmask="255.255.0.0"
+grid2_netmask_mgmt="255.255.0.0"
+grid2_router="10.35.0.1"
+grid2_router_mgmt="10.36.0.1"
+grid2_router_ipv6="2001:0550:040A:2400:0000:0000:0000:0001"
+grid2_router_ipv6_mgmt="2001:0550:040A:2500:0000:0000:0000:0001"
+
+#ADDING VALUES OF EXTERNAL SERVERS BELOW
+ms_server1_ip="10.34.98.33"
+dns_resolver_ip="10.39.16.160"
+ms_server2_ip="10.34.98.34"
+
+
+#ADDING VALUES OF CUSTOM VARIABLES BELOW
+username="admin"
+tftp_client="10.36.199.5"
+password="infoblox"
+wapi_version="2.12.2"
+grid_vip="10.36.160.18"
+client_ip="10.36.198.8"
+client_user="test1"
